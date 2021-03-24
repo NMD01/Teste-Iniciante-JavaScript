326bae3272285f76fea151b536032131465cfa19
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-03-24 20:00:16 -0300

    Add files via upload

---
+++ tes010.js
@@ -0,0 +1,13 @@
+var idade = 20
+console.log(`Vc tem ${idade} anos`)
+if(idade <= 15){
+    console.log('Não vota')
+
+}else if(idade > 15 && idade < 18 || idade >= 67){
+    console.log('voto opcional')
+
+   }else{ 
+console.log('voto Obrigatorio')}
+    
+
+
